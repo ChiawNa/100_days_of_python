8f5a6f3f18873c468aefa4a856535142b8a84578
ChiawNa <chiawnak0390@gmail.com>
2024-05-12 01:07:27 +0800

    L4 Mathematical Operations

--- Day 2 - Data types/main.py
+++ Day 2 - Data types/main.py
@@ -1,47 +1,73 @@
-print("Lesson 1:- Python Primitive Data Types\n")
+# print("Lesson 1:- Python Primitive Data Types\n")
 
-print("Hello"[0]) #subcript
+# print("Hello"[0]) #subcript
 
-#Integer
-print(123 + 456)
+# #Integer
+# print(123 + 456)
 
-#Float
-3.14159
+# #Float
+# 3.14159
 
-#Boolean
-True
-False
+# #Boolean
+# True
+# False
 
 
-print("\nLesson 2:- Type Error, Type Checking and Type Conversion\n")
-#below code has error (type error: int->str)
+# print("\nLesson 2:- Type Error, Type Checking and Type Conversion\n")
+# #below code has error (type error: int->str)
+
+# # num_char = len(input("What is your name?\n"))
+# # print("your name has " + num_char + "characters.\n")
+
+
+# #solution
 
 # num_char = len(input("What is your name?\n"))
-# print("your name has " + num_char + "characters.\n")
+# print(type(num_char))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + "characters.\n")
+
+# a = 123
+# print(type(a))
+
+# a = str(123)
+# print(type(a))
+
+# print(70 + float("100.5"))
+# print(str(70)+str(100))
 
 
-#solution
+# print("Lesson 3:- [Interactive Coding Exercise] Data Types")
 
-num_char = len(input("What is your name?\n"))
-print(type(num_char))
-new_num_char = str(num_char)
-print("Your name has " + new_num_char + "characters.\n")
+# #Write a program that adds the digits in a 2 digit number. 
+# #e.g. if the input was 35, then the output should be 3 + 5 = 8
 
-a = 123
-print(type(a))
-
-a = str(123)
-print(type(a))
-
-print(70 + float("100.5"))
-print(str(70)+str(100))
+# two_digit_number = input()
+# #print(type(two_digit_number))
+# print(int(two_digit_number[0]) + int(two_digit_number[1]))
 
 
-print("Lesson 3:- [Interactive Coding Exercise] Data Types")
+print("Lesson 4:- Mathematical Operations")
 
-#Write a program that adds the digits in a 2 digit number. 
-#e.g. if the input was 35, then the output should be 3 + 5 = 8
+# PEMDASLR
+# ()
+# **
+# */
+# +-
 
-two_digit_number = input()
-#print(type(two_digit_number))
-print(int(two_digit_number[0]) + int(two_digit_number[1]))
+#print(3*(3+3)/3-3)
+
+
+#Write a program that calculates the Body Mass Index (BMI) 
+#from a user's weight and height.
+
+height = input()
+weight = input()
+
+float_height = float(height)
+int_weight = int(weight)
+
+BMI = (int_weight/(float_height*float_height))
+int_BMI = int(BMI)
+
+print(int_BMI)
