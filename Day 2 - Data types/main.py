7a4fa67ca5e2a09c83c62b0809fa400ff0644257
ChiawNa <chiawnak0390@gmail.com>
2024-05-13 00:14:48 +0800

    L5 - Number Manipulation and F Strings

--- Day 2 - Data types/main.py
+++ Day 2 - Data types/main.py
@@ -47,7 +47,7 @@
 # print(int(two_digit_number[0]) + int(two_digit_number[1]))
 
 
-print("Lesson 4:- Mathematical Operations")
+# print("Lesson 4:- Mathematical Operations")
 
 # PEMDASLR
 # ()
@@ -61,13 +61,35 @@ print("Lesson 4:- Mathematical Operations")
 #Write a program that calculates the Body Mass Index (BMI) 
 #from a user's weight and height.
 
-height = input()
-weight = input()
+# height = input()
+# weight = input()
 
-float_height = float(height)
-int_weight = int(weight)
+# float_height = float(height)
+# int_weight = int(weight)
 
-BMI = (int_weight/(float_height*float_height))
-int_BMI = int(BMI)
+# BMI = (int_weight/(float_height*float_height))
+# int_BMI = int(BMI)
 
-print(int_BMI)
+# print(int_BMI)
+
+
+print("Lesson 5:- Number Manipulation and F Strings\n")
+
+print(round(2.6666, 2))
+print(8//3) #answer in int
+print(8/3) #answer in float
+
+# f string (to combine dff data type)
+
+score = 0 
+height = 1.8
+isWinning = True
+print(f"your score is {score}, your height is {height}, your are winning is {isWinning}")
+
+#Create a program using maths and f-Strings that tells us how many weeks we have left, 
+#if we live until 90 years old.
+
+# A years have 52 weeks.
+age = input()
+x = (90 - int(age)) * 52
+print(f"You have {x} weeks left.")
