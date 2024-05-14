d2dd4356a722656a081f7fef303b6cc999f254ec
ChiawNa <chiawnak0390@gmail.com>
2024-05-14 21:57:47 +0800

    Nested if and elif statements

--- Day 3 - Control Flow and Logical Operators/main.py
+++ Day 3 - Control Flow and Logical Operators/main.py
@@ -1,20 +1,60 @@
-print("Lesson1 :- Control Flow with if/else and Conditional Operators\n")
+# print("Lesson1 :- Control Flow with if/else and Conditional Operators\n")
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+# if height > 120:
+#     print("You can ride the rollercoaster.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+# #Write a program that works out whether if a given number is an odd or even number.
+# #Even numbers can be divided by 2 with no remainder.
+
+# number = int(input())
+
+# if number % 2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+
+print("Lesson2 :- Nested if and elif statements\n")
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
 if height > 120:
     print("You can ride the rollercoaster.")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5.")
+    elif 12<age<18: #elif age <=18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
 
-#Write a program that works out whether if a given number is an odd or even number.
-#Even numbers can be divided by 2 with no remainder.
+#Write a program that interprets the Body Mass Index (BMI) 
+#based on a user's weight and height.
 
-number = int(input())
+# Enter your height in meters e.g., 1.55
+height = float(input())
+# Enter your weight in kilograms e.g., 72
+weight = int(input())
 
-if number % 2 == 0:
-    print("This is an even number.")
+BMI = float(weight/(height*height))
+
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI < 25:
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI < 30:
+    print(f"Your BMI is {BMI}, you are slightly overweight") 
+elif BMI < 35:
+    print(f"Your BMI is {BMI}, you are obese.")
 else:
-    print("This is an odd number.")
+    print(f"Your BMI is {BMI}, you are clinically obese.")
