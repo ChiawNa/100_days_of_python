b083a50446689385cc8743a9c0641f54bb947cd2
ChiawNa <chiawnak0390@gmail.com>
2024-05-17 00:57:56 +0800

    Python Pizza with multiple If

--- Day 3 - Control Flow and Logical Operators/main.py
+++ Day 3 - Control Flow and Logical Operators/main.py
@@ -76,30 +76,67 @@
 # else:
 #     print("Not leap year")
 
-print("Lesson3 :- Multiple If statements\n")
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Lesson3 :- Multiple If statements\n")
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# bill = 0
+
+# if height > 120:
+#     print("You can ride the rollercoaster.")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         bill = 5
+#         print("Please pay $5.")
+#     elif age <18:
+#         bill = 7
+#         print("Please pay $7.")
+#     else:
+#         bill = 12
+#         print("Please pay $12.")
+
+#     want_photo = input("Do you want a photo taken? Y or N. ")
+#     if want_photo == "Y":
+#         bill += 3
+
+#     print(f"Your final bill is {bill}")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+    
+# Based on a user's order, work out their final bill.
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+# Add extra cheese for any size pizza (Y or N): +$1
+
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input() # What size pizza do you want? S, M, or L
+add_pepperoni = input() # Do you want pepperoni? Y or N
+extra_cheese = input() # Do you want extra cheese? Y or N
+
 bill = 0
 
-if height > 120:
-    print("You can ride the rollercoaster.")
-    age = int(input("What is your age? "))
-    if age < 12:
-        bill = 5
-        print("Please pay $5.")
-    elif age <18:
-        bill = 7
-        print("Please pay $7.")
-    else:
-        bill = 12
-        print("Please pay $12.")
+if size == "S":
+    bill = 15
 
-    want_photo = input("Do you want a photo taken? Y or N. ")
-    if want_photo == "Y":
+    if add_pepperoni == "Y":
+        bill += 2
+
+elif size == "M":
+    bill = 20
+
+else:
+    bill = 25
+
+if add_pepperoni == "Y":
         bill += 3
 
-    print(f"Your final bill is {bill}")
-else:
-    print("Sorry, you have to grow taller before you can ride.")
-    
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
