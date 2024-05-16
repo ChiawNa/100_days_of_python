9d51517bbdc9d02c3c04794b3a7d2eadb402e255
ChiawNa <chiawnak0390@gmail.com>
2024-05-17 00:29:11 +0800

    Multiple If Statement

--- Day 3 - Control Flow and Logical Operators/main.py
+++ Day 3 - Control Flow and Logical Operators/main.py
@@ -63,15 +63,43 @@
 #A normal year has 365 days, leap years have 366, with an extra day in February. 
 
 
-year = int(input())
+# year = int(input())
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap year")
-        else:
-            print("Not leap year")
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year")
+#         else:
+#             print("Not leap year")
+#     else:
+#         print("Lear year")
+# else:
+#     print("Not leap year")
+
+print("Lesson3 :- Multiple If statements\n")
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height > 120:
+    print("You can ride the rollercoaster.")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Please pay $5.")
+    elif age <18:
+        bill = 7
+        print("Please pay $7.")
     else:
-        print("Lear year")
+        bill = 12
+        print("Please pay $12.")
+
+    want_photo = input("Do you want a photo taken? Y or N. ")
+    if want_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is {bill}")
 else:
-    print("Not leap year")
+    print("Sorry, you have to grow taller before you can ride.")
+    
