c194dbc477f7fda0e1bfea2a4c4ce769ae429b70
ChiawNa <chiawnak0390@gmail.com>
2024-05-26 23:27:01 +0800

    For loop and Range() function

--- Day 5 - Python Loop/main.py
+++ Day 5 - Python Loop/main.py
@@ -1,25 +1,26 @@
-# print("Lesson 1:- For loop with Python Lists\n")
+print("Lesson 1:- For loop with Python Lists\n")
 
-# # You are going to write a program that calculates the average student height from a List of heights.
+# You are going to write a program that calculates the average student height from a List of heights.
 
-# # Input a Python list of student heights
-# student_heights = input().split()
-# for n in range(0, len(student_heights)):
-#   student_heights[n] = int(student_heights[n])
+# Input a Python list of student heights
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
   
-# total_height = 0
-# for height in student_heights:
-#   total_height += height
+total_height = 0
+for height in student_heights:
+  total_height += height
 
-# total_student = 0
-# for student in student_heights:
-#   total_student += 1
+total_student = 0
+for student in student_heights:
+  total_student += 1
 
-# average_height = round(total_height / total_student)
+average_height = round(total_height / total_student)
 
-# print(f"\ntotal height = {total_height}")
-# print(f"number of students = {total_student}")
-# print(f"average height = {average_height}")
+print(f"\ntotal height = {total_height}")
+print(f"number of students = {total_student}")
+print(f"average height = {average_height}")
+
 
 
 # You are going to write a program that calculates the highest score from a List of scores.
@@ -36,3 +37,17 @@ for score in student_scores:
     max_score = score
 
 print(f"The highest score in the class is: {max_score}")
+
+
+print("Lesson 2:- For loop and Range() function\n")
+
+# You are going to write a program that calculates the sum of all the even numbers from 1 to X. 
+# If X is 100 then the first even number would be 2 and the last one is 100:
+# i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+
+target = int(input()) # Enter a number between 0 and 1000
+
+total = 0
+for i in range(2, target+1, 2):
+    total = total + i
+print(total)
