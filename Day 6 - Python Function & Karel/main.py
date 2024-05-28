2d63ac9802df24644b333945387d0cecb6a9ddda
ChiawNa <chiawnak0390@gmail.com>
2024-05-29 00:51:11 +0800

    Project 6 - Escaping the Maze

--- Day 6 - Python Function & Karel/main.py
+++ Day 6 - Python Function & Karel/main.py
@@ -97,3 +97,23 @@ while at_goal() != True:
         all()
     else:
         move()
+
+
+# Reeborg was exploring a dark maze and the battery in its flashlight ran out.
+# Write a program using an if/elif/else statement so Reeborg can find the exit. 
+# The secret is to have Reeborg follow along the right edge of the maze, turning right if it can, going straight ahead if it can’t turn right, or turning left as a last resort.
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while at_goal() != True:
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
