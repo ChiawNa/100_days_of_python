fcea88b54f977704bfbd676f4107e31eb59d35ef
ChiawNa <chiawnak0390@gmail.com>
2024-05-29 00:25:50 +0800

    Hurdles Challenges using while and for loops

--- Day 6 - Python Function & Karel/main.py
+++ Day 6 - Python Function & Karel/main.py
@@ -22,6 +22,7 @@ for step in range(6):
 
 # Reeborg has entered a hurdle race, but he does not know in advance how long the race is. 
 # Make him run the course, following a path similar to the one shown, but stopping at the only flag that will be shown after the race has started.
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json#
 
 def turn_right():
     turn_left()
@@ -40,3 +41,59 @@ def all():
 
 while at_goal() != True:
     all()
+
+
+# Reeborg has entered a hurdle race. Make him run the course, following the path shown.
+# The position and number of hurdles changes each time this world is reloaded.
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def all():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() != True:
+    if wall_in_front():
+        all()
+    else:
+        move()
+
+
+# Reeborg has entered a hurdle race. Make him run the course, following the path shown.
+# The position, the height and the number of hurdles changes each time this world is reloaded.
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def all():
+    turn_left()
+    
+    while wall_on_right():
+        move()
+  
+    turn_right()
+    move()
+    turn_right()
+    
+    while front_is_clear():
+        move()
+
+    turn_left()
+
+while at_goal() != True:
+    if wall_in_front():
+        all()
+    else:
+        move()
