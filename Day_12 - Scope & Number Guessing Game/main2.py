591f17d78998dc4925bf95eaa0661ce10e6c4263
ChiawNa <chiawnak0390@gmail.com>
2024-06-26 23:31:11 +0800

    Number Guessing Game

---
+++ Day_12 - Scope & Number Guessing Game/main2.py
@@ -0,0 +1,45 @@
+import random
+from art import logo 
+
+def play_game(attempt):
+
+    num = range(1,101)
+    number = random.choice(num)
+    finish_guess = False
+
+    while attempt > 0 and not finish_guess:
+        print(f"You have {attempt} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+
+        if guess > number:
+            print("Too high.")
+        
+        elif guess < number:
+            print("Too low.")
+        
+        elif guess == number:
+            print("Correct answer!")
+            finish_guess = True
+
+        attempt -= 1
+
+        if attempt > 0 and not finish_guess:
+            print("Guess again.")
+
+    if not finish_guess:
+        print(f"Sorry, you've run out of attempts. The correct answer is {number}.")
+
+
+print(logo)
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard: ")
+
+if difficulty == 'easy':
+    play_game(10)
+elif difficulty == 'hard':
+    play_game(5)
+
---
+++ Day_12 - Scope & Number Guessing Game/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+        # finish_guess == True
