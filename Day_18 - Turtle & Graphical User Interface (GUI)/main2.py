68f6d6aa2b6947a2b5eff8a8b3359b9727f5f7eb
ChiawNa <chiawnak0390@gmail.com>
2024-10-02 22:34:09 +0800

    generate random RGB colors

--- Day_18 - Turtle & Graphical User Interface (GUI)/main2.py
+++ Day_18 - Turtle & Graphical User Interface (GUI)/main2.py
@@ -1,15 +1,37 @@
-from turtle import Turtle
+# from turtle import Turtle
+# import random
+
+# tim = Turtle()
+# directions = [0, 90, 180, 270]  # Possible directions: right, up, left, down
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+# tim.pensize(10)
+# tim.speed(10)
+
+# for _ in range(200):
+#     tim.color(random.choice(colours))
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
+import turtle as t 
 import random
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)    
+    b = random.randint(0, 255)
+    
+    random_color = (r,g,b)
+    return random_color
+
+
 directions = [0, 90, 180, 270]  # Possible directions: right, up, left, down
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 tim.pensize(10)
 tim.speed(10)
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
-
-
