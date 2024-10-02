2207cf657c48e8041ecfdcd1c24c80d22da84ce5
ChiawNa <chiawnak0390@gmail.com>
2024-10-03 01:12:33 +0800

    draw the dots

--- Day_18 - Turtle & Graphical User Interface (GUI)/hirst-painting-dots.py
+++ Day_18 - Turtle & Graphical User Interface (GUI)/hirst-painting-dots.py
@@ -1,19 +1,50 @@
-import colorgram
+# Extract rgb color from image
 
-# Extract 6 colors from an image.
-colors = colorgram.extract('C:\\Users\ASUS\OneDrive\\100 days of python\\Day_18 - Turtle & Graphical User Interface (GUI)\\hirst_painting_dot.jpg', 32)
+# import colorgram
 
-# Create a list of tuples containing the RGB values
-color_tuples = []
+# # Extract 6 colors from an image.
+# colors = colorgram.extract('C:\\Users\ASUS\OneDrive\\100 days of python\\Day_18 - Turtle & Graphical User Interface (GUI)\\hirst_painting_dot.jpg', 32)
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r,g,b)
-    color_tuples.append(new_color)
+# # Create a list of tuples containing the RGB values
+# color_tuples = []
 
-# Print the list of color tuples
-print(color_tuples)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     color_tuples.append(new_color)
 
-color_list = [(84, 254, 155), (173, 146, 118), (254, 250, 254), (245, 39, 191), (158, 107, 56), (2, 1, 176), (151, 54, 251), (221, 254, 101), (253, 146, 193), (3, 87, 176), (249, 1, 246), (35, 34, 253), (1, 213, 212), (249, 0, 0), (254, 147, 146), (253, 71, 70), (244, 248, 254), (39, 249, 42), (85, 249, 253), (240, 1, 13), (5, 210, 216), (230, 126, 190), (2, 2, 107), (135, 152, 220), (174, 162, 249), (208, 118, 26), (253, 7, 4), (248, 6, 19)]
+# # Print the list of color tuples
+# print(color_tuples)
+
+import turtle as turtle_module
+import random
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed(0)
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+color_list = [(84, 254, 155), (173, 146, 118), (245, 39, 191), (158, 107, 56), (2, 1, 176), (151, 54, 251), (221, 254, 101), (253, 146, 193), (3, 87, 176), (249, 1, 246), (35, 34, 253), (1, 213, 212), (249, 0, 0), (254, 147, 146), (253, 71, 70), (39, 249, 42), (85, 249, 253), (240, 1, 13), (5, 210, 216), (230, 126, 190), (2, 2, 107), (135, 152, 220), (174, 162, 249), (208, 118, 26), (253, 7, 4), (248, 6, 19)]
+
+
+for dots_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dots_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = turtle_module.Screen()
+screen.exitonclick()
+
