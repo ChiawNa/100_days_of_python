80596b8ccb08eb4a5bcb59ea2c5744d8f25adf77
ChiawNa <chiawnak0390@gmail.com>
2024-10-02 18:12:53 +0800

    turtle challenge 3 draw the different shapes

--- Day_18 - Turtle & Graphical User Interface (GUI)/main.py
+++ Day_18 - Turtle & Graphical User Interface (GUI)/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
+import random
 
-timmy_the_turtle = Turtle()
+tim = Turtle()
 
 # draw a square
 # for i in range(4):
@@ -16,53 +17,68 @@ timmy_the_turtle = Turtle()
 #     timmy_the_turtle.pendown()
 
 
-# draw a triangle
-for _ in range(3):
-        timmy_the_turtle.color("red")
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(120)
+# # draw a triangle
+# for _ in range(3):
+#         timmy_the_turtle.color("red")
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(120)
 
-# draw a square
-for i in range(4):
-    timmy_the_turtle.color("orange")
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+# # draw a square
+# for i in range(4):
+#     timmy_the_turtle.color("orange")
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
 
-# draw a pentagon
-for i in range(5):
-    timmy_the_turtle.color("yellow")
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(72)
+# # draw a pentagon
+# for i in range(5):
+#     timmy_the_turtle.color("yellow")
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(72)
 
-# draw a hexagon
-for i in range(6):
-    timmy_the_turtle.color("green")
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(60)
+# # draw a hexagon
+# for i in range(6):
+#     timmy_the_turtle.color("green")
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(60)
 
-# draw a heptagon
-for i in range(7):
-    timmy_the_turtle.color("blue")
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(360/7)
+# # draw a heptagon
+# for i in range(7):
+#     timmy_the_turtle.color("blue")
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(360/7)
 
-# draw a octagon
-for i in range(8):
-    timmy_the_turtle.color("violet")
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(45)
+# # draw a octagon
+# for i in range(8):
+#     timmy_the_turtle.color("violet")
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(45)
 
-# draw a nanogon
-for i in range(9):
-    timmy_the_turtle.color("black")
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(40)
+# # draw a nanogon
+# for i in range(9):
+#     timmy_the_turtle.color("black")
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(40)
 
-# draw a decagon
-for i in range(10):
-    timmy_the_turtle.color("pink")
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(36)
+# # draw a decagon
+# for i in range(10):
+#     timmy_the_turtle.color("pink")
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(36)
+
+
+# draw different shapes
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
