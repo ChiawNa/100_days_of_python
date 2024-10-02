63d8ad355b12f652bf0b9e7f5f61f927f675ca12
ChiawNa <chiawnak0390@gmail.com>
2024-10-02 23:02:37 +0800

    turtle challenge 5 - draw a spirograph

--- Day_18 - Turtle & Graphical User Interface (GUI)/main2.py
+++ Day_18 - Turtle & Graphical User Interface (GUI)/main2.py
@@ -1,3 +1,5 @@
+# generate random walk using selected colors
+
 # from turtle import Turtle
 # import random
 
@@ -12,7 +14,38 @@
 #     tim.forward(30)
 #     tim.setheading(random.choice(directions))
 
-import turtle as t 
+
+
+# generate random walk using random RGB colors 
+
+# import turtle as t 
+# import random
+
+# tim = t.Turtle()
+# t.colormode(255)
+
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)    
+#     b = random.randint(0, 255)
+    
+#     random_color = (r,g,b)
+#     return random_color
+
+
+# directions = [0, 90, 180, 270]  # Possible directions: right, up, left, down
+# tim.pensize(10)
+# tim.speed(10)
+
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
+
+# create a spirograph
+
+import turtle as t
 import random
 
 tim = t.Turtle()
@@ -23,15 +56,18 @@ def random_color():
     g = random.randint(0, 255)    
     b = random.randint(0, 255)
     
-    random_color = (r,g,b)
-    return random_color
+    color = (r,g,b)
+    return color
 
 
-directions = [0, 90, 180, 270]  # Possible directions: right, up, left, down
-tim.pensize(10)
-tim.speed(10)
+tim.speed(0)
 
-for _ in range(200):
+for _ in range(100):
     tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+    tim.circle(100)
+    current_heading = tim.heading()
+    tim.setheading(current_heading + 10)
+    tim.circle(100)
+
+screen = t.Screen()
+screen.exitonclick
