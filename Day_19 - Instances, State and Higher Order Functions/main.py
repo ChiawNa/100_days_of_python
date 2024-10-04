68c0a1f8bf5bc58e8817eeced814f71eb6c8af23
ChiawNa <chiawnak0390@gmail.com>
2024-10-05 01:00:17 +0800

    Etch-A-Sketch App

--- Day_19 - Instances, State and Higher Order Functions/main.py
+++ Day_19 - Instances, State and Higher Order Functions/main.py
@@ -6,8 +6,24 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def counter_clockwise():
+    tim.left(10)
+
+def clockwise():
+    tim.right(10)
+
+def clear():
+    tim.reset()
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(counter_clockwise, "a")
+screen.onkey(clockwise, "d")
+screen.onkey(clear, "c")
 
 
 screen.exitonclick()
