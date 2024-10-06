7ac361b08037ab046f62f7079562c3d10ce5aa14
ChiawNa <chiawnak0390@gmail.com>
2024-10-07 01:24:13 +0800

    move the snake

--- Day_20 - Snake Game Part 1_Animation & Coordinates/main.py
+++ Day_20 - Snake Game Part 1_Animation & Coordinates/main.py
@@ -1,15 +1,38 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Snake Game")
-position = [(0,0), (-20,0), (-40,0)]
+screen.tracer(0)
 
-for i in range(0,3):
+
+position = [(0,0), (-20,0), (-40,0)]
+all_segments = []
+
+
+# Create 3 segments
+for i in range(0,3): # 0 - 2
     segment = Turtle("square")
     segment.color("white")
+    segment.penup()
     segment.goto(position[i])
+    all_segments.append(segment)
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+# moves the segments in reverse order (from the last to the first)
+    for seg_num in range(len(all_segments)-1, 0, -1): # for seg_num in range(2, 0, -1)
+        new_x = all_segments[seg_num-1].xcor()
+        new_y = all_segments[seg_num-1].ycor()
+        all_segments[seg_num].goto(new_x, new_y)
+
+    all_segments[0].forward(20)
 
 # segment_1 = Turtle("square")
 # segment_1.color("white")
@@ -21,8 +44,6 @@ for i in range(0,3):
 # segment_3 = Turtle("square")
 # segment_3.color("white")
 # segment_3.goto(-40, 0)
-
-
 
 
 screen.exitonclick()
